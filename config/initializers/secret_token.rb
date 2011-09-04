7c560bf7c484dd7dc6fd078b4da5b9c552157fa8
krishna rokhale <krishna.rokhale@gmail.com>
2011-09-03 18:15:43 -0700

    bootstrapping!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scribles::Application.config.secret_token = '99e1f7b3448f03dea6b03a81c29dc0f5595ebff57cd8b8c09eef69e4e4e49275bb726733ae1f44082513a1cb6948ffe0a40ce43680ed4f3e5f17501e202a28a4'
